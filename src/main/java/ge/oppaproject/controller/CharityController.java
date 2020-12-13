16a4b80bde1ba41906160a3f4930568a5ebfa533
LordCommander21 <gogadzedaniel21@gmail.com>
2020-12-13 18:56:45 +0400

    viewRefactoring

--- src/main/java/ge/oppaproject/controller/CharityController.java
+++ src/main/java/ge/oppaproject/controller/CharityController.java
@@ -2,11 +2,20 @@ package ge.oppaproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CharityController {
     @GetMapping("/charity")
     public String charity(){
         return "charity";
+    }
+
+    @PostMapping("/charity")
+    public String charity(@RequestParam String privateId,
+                          @RequestParam String phoneNumber,
+                          @RequestParam String amount){
+        return "redirect:/";
     }
 }
--- src/main/java/ge/oppaproject/controller/UtilityController.java
+++ src/main/java/ge/oppaproject/controller/UtilityController.java
@@ -2,11 +2,19 @@ package ge.oppaproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UtilityController {
     @GetMapping("/utility")
     public String utility(){
         return "utility";
+    }
+    @PostMapping("/utility")
+    public String utility(@RequestParam String privateId,
+                          @RequestParam String phoneNumber,
+                          @RequestParam String amount){
+        return "redirect:/";
     }
 }
